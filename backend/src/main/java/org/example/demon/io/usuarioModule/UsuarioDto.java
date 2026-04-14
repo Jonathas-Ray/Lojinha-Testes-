898029bdb6dfe10c9fbbd0a092b503ab01e4e99a
Heitor Trindade <heitortrindade1414@gmail.com>
2026-04-14 06:05:40 -0300

    adicionadas interfaces Repositories e Dto

---
+++ backend/src/main/java/org/example/demon/io/usuarioModule/UsuarioDto.java
@@ -0,0 +1,26 @@
+package org.example.demon.io.usuarioModule;
+
+public class UsuarioDto {
+    private String nome;
+    private String email;
+
+    public usuarioDto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
