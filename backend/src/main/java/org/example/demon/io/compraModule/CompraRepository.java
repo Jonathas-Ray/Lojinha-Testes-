56401f9463da323b83042ebd80999c8f6d08cc43
Jonathas-Ray <jonathasrmcguimaraes@gmail.com>
2026-04-15 20:24:32 -0300

    Pecas faltantes

---
+++ backend/src/main/java/org/example/demon/io/compraModule/CompraRepository.java
@@ -0,0 +1,9 @@
+package org.example.demon.io.compraModule;
+
+import org.example.demon.io.models.Compra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompraRepository extends JpaRepository<Compra, Integer> {
+}
