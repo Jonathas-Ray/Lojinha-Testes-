9c0a8b578ba5203256269801f136cee018365485
Heitor Trindade <heitortrindade1414@gmail.com>
2026-04-15 06:14:31 -0300

    alterações que o github nao pegou antes

--- backend/src/main/java/org/example/demon/io/compraModule/CompraService.java
+++ backend/src/main/java/org/example/demon/io/compraModule/CompraService.java
@@ -17,42 +17,37 @@ public class CompraService {
     private CompraRepository compraRepository;
 
     @Autowired
-    private ProdutoRepository produtoRepository; // Precisamos para buscar os produtos
+    private ProdutoRepository produtoRepository;
 
     @Autowired
     private UsuarioService usuarioService;
 
     @Transactional
     public void finalizarCompra(List<Integer> produtosIds) {
-        // 1. Identificar o comprador pelo Token
+
         String emailLogado = SecurityContextHolder.getContext().getAuthentication().getName();
         Usuario comprador = usuarioService.buscarPorEmail(emailLogado);
 
-        // 2. Criar a instância da Compra e associar o comprador
         Compra compra = new Compra();
         compra.setComprador(comprador);
 
-        // 3. Buscar os produtos no banco pelos IDs enviados
         List<Produto> produtosParaComprar = produtoRepository.findAllById(produtosIds);
 
         if (produtosParaComprar.isEmpty()) {
             throw new RuntimeException("O carrinho está vazio ou os produtos não existem!");
         }
 
-        // 4. Vincular cada produto à compra (IMPORTANTE para o JPA)
         for (Produto produto : produtosParaComprar) {
-            // Validação opcional: O produto já foi comprado?
+
             if (produto.getCompra() != null) {
                 throw new RuntimeException("O produto " + produto.getNome() + " já foi vendido!");
             }
 
-            produto.setCompra(compra); // Seta o @ManyToOne no lado do Produto
+            produto.setCompra(compra);
         }
 
-        // 5. Adicionar a lista de produtos à compra
         compra.setProdutos(produtosParaComprar);
 
-        // 6. Salvar (O CascadeType.ALL vai cuidar de atualizar os produtos)
         compraRepository.save(compra);
     }
 }
